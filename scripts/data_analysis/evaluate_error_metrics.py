fixing covariance tuning

scripts/data_analysis/evaluate_error_metrics.py
@@ -30,7 +30,7 @@ COLUMN_NAMES_IDX = ['conf_names', 'conf_names_filt', 'conf_names_vel',
 
 # Select which instructions to consider
 SELECTED_INSTRUCTIONS = {
-    'PICK-&-PLACE': [1],#[1, 2, 3, 4, 5, 6, 7, 8], # discard instruction_id=0, as the subject stands still
+    'PICK-&-PLACE': [3],#[1, 2, 3, 4, 5, 6, 7, 8], # discard instruction_id=0, as the subject stands still
     'WALKING':      [1, 2, 3],                # discard instruction_id=0, as the subject stands still
     'PASSING-BY':   [1, 2] # do not select instruction_id=0, as the subject is outside the camera view
 }                                                 
@@ -74,19 +74,26 @@ DIMENSIONS_PER_KEYPOINT = {0:  ['y'],
                            8:  ['y'],
                            11: ['y']}
 
-# Define possible prediction horizons (in steps)
-PRED_HORIZONS = [1, 3, 5]
+# Define possible prediction horizons (in seconds)
+PRED_HORIZONS = [0.1, 0.3, 0.5]
 
 # Define whether to load the results from the pickle files
 LOADING_RESULTS = False
 
+# Define whether to tune the variance parameters
+TUNE_INIT_VARIANCE_JOINTS = True
+NUM_MONTECARLO_SAMPLES = 10000
+
 # Define the space in which the filter operates
-SPACE = 'cartesian'                             # 'cartesian' or 'joint'
+SPACE = 'joint'                             # 'cartesian' or 'joint'
 # Define the space in which the error metrics are evaluated
 SPACE_FOR_EVALUATION = 'cartesian'              # 'cartesian' or 'joint'
 
 assert SPACE_FOR_EVALUATION == 'cartesian' if SPACE == 'cartesian' else True, \
     "SPACE_FOR_EVALUATION must be 'cartesian' if SPACE is 'cartesian'."
+
+assert SPACE == 'joint' and not LOADING_RESULTS if TUNE_INIT_VARIANCE_JOINTS else True, \
+    "SPACE must be 'joint' and LOADING_RESULTS must be False if TUNE_INIT_VARIANCE_JOINTS is True."
 
 # Define the number of keypoints and joints
 N_KPTS = 13
@@ -107,24 +114,29 @@ N_DIM_PER_KPT = 3                               # x, y, z
 MAX_TIME_NO_MEAS = pd.Timedelta(seconds= 0.1)   # maximum time without measurements before resetting the filter
 
 # Initial uncertainty parameters for the filters
-VAR_MEAS_KPT = 0.0025           # [paper: r_y] Hip: 3-sigma (99.5%) = 0.15 m ==> sigma 0.05 m ==> var = (0.05)^2 m^2
-VAR_Q_ACC = 0.01                # [paper: q_a] a_dot = u (u = 0 is very uncertain ==> add variance here)
+VAR_MEAS_KPT = 0.05**2           # [paper: r_y] Hip: 3-sigma (99.5%) = 0.15 m ==> sigma 0.05 m ==> var = (0.05)^2 m^2
+VAR_Q_ACC = 0.01                 # [paper: q_a] [TUNABLE] a_dot = u (u = 0 is very uncertain ==> add variance here)
 
 if SPACE == 'cartesian':
     INIT_VAR_POS = VAR_MEAS_KPT     # [paper: p_y] Set equal to the measurement noise since the state is initialized with the measurement
-    INIT_VAR_VEL = 0.02844          # [paper: p_v] Hip: no keypoint moves faster than 1.6 m/s ==> 3-sigma (99.5%) = 1.6 m/s ==> var = (1.6/3)^2 m^2/s^2
-    INIT_VAR_ACC = 1.1111           # [paper: p_a] Hip: no keypoint accelerates faster than 10 m/s^2 ==> 3-sigma (99.5%) = 10 m/s^2 ==> var = (10/3)^2 m^2/s^4
+    INIT_VAR_VEL = (1.6/3.0)**2     # [paper: p_v] Hip: no keypoint moves faster than 1.6 m/s ==> 3-sigma (99.5%) = 1.6 m/s ==> var = (1.6/3)^2 m^2/s^2
+    INIT_VAR_ACC = (1.6/3.0)**2     # [paper: p_a] Hip: no keypoint accelerates faster than 10 m/s^2 ==> 3-sigma (99.5%) = 10 m/s^2 ==> var = (10/3)^2 m^2/s^4
 elif SPACE == 'joint':
     INIT_VAR_POS = 0.01 #TODO: to tune -> should be related to VAR_MEAS_KPT, but the link is the NONLINEAR inverse kinematics => not trivial, requires MonteCarlo?
                         # Moreover: which subject to use for the MonteCarlo? All subjects? Only the training subjects? The body parameters are different for each subject.
-    INIT_VAR_VEL = 0.01 #TODO: to tune
-    INIT_VAR_ACC = 0.01 #TODO: to tune
     INIT_VAR_PARAM = 0.1 #TODO: to tune
+    UNCERTAINTY_FACTOR = 10.0                        #TODO: check, arbitrary choice
+    INIT_VAR_VEL = UNCERTAINTY_FACTOR * INIT_VAR_POS #TODO: check, arbitrary choice
+    INIT_VAR_ACC = UNCERTAINTY_FACTOR * INIT_VAR_VEL #TODO: check, arbitrary choice
 else:
     ValueError("SPACE must be either 'cartesian' or 'joint'.")
 
 # Parameters for the IMM estimator
 USE_SINDY_MODEL = False
+
+assert SPACE == 'joint' if USE_SINDY_MODEL else True, \
+    "SPACE must be 'joint' if USE_SINDY_MODEL is True."
+
 NUM_FILTERS_IN_BANK = 3
 M = np.array([[0.55, 0.15, 0.30], # transition matrix for the IMM estimator
               [0.15, 0.75, 0.10],
@@ -213,6 +225,8 @@ elif SPACE == 'joint':
 else:
     raise ValueError("SPACE must be either 'cartesian' or 'joint'.")
 
+# Define the number of steps based on PRED_HORIZONS and DT
+num_pred_steps = [int(np.round(hor / DT)) for hor in PRED_HORIZONS]
 
 # Define the dimensionality of the state space for the filter
 if SPACE == 'cartesian':
@@ -289,10 +303,27 @@ if not LOADING_RESULTS:
             SELECTED_TASK_NAMES,
             selected_columns # measurement data
         )
+    
+
+    tic = time.time()
+    # Possibly perform tuning of the variance parameters
+    if SPACE == 'joint' and TUNE_INIT_VARIANCE_JOINTS:
+        print("\nTuning the initial variance using the IDENTIFICATION subjects...")
+        INIT_VAR_POS, INIT_VAR_PARAM = ukf_predictor.tune_init_variance_joints(
+            X_train_list, time_train_list, train_traj_idx,
+            DT, SELECTED_KPT_NAMES, SELECTED_KEYPOINTS_FOR_KINEMATIC_MODEL,
+            N_JOINTS, N_PARAM, NUM_MONTECARLO_SAMPLES, VAR_MEAS_KPT
+        )
+
+        INIT_VAR_VEL = UNCERTAINTY_FACTOR * INIT_VAR_POS #TODO: check, arbitrary choice
+        INIT_VAR_ACC = UNCERTAINTY_FACTOR * INIT_VAR_VEL #TODO: check, arbitrary choice
+
+        toc = time.time() - tic
+        minutes, seconds = divmod(toc, 60)
+        print(f"Variance parameters tuning took {minutes:.0f} minutes and {seconds:.2f} seconds.")
 
 
     print("\nInitialize the filter...")
-
     # Initialize the state covariance matrix
     if SPACE == 'cartesian':
         init_P = ukf_predictor.initialize_P(N_VAR_PER_KPT, N_DIM_PER_KPT, N_KPTS, INIT_VAR_POS, INIT_VAR_VEL, INIT_VAR_ACC)
@@ -304,10 +335,9 @@ if not LOADING_RESULTS:
 
 
     print("\nRun the filtering loop for all subjects, identification and validation...")
-
     tic = time.time()
 
-    # Initialize the SINDy model
+    # Possibly initialize the SINDy model
     sindy_model = None
     if USE_SINDY_MODEL:
         # Load SINDy model
@@ -323,10 +353,15 @@ if not LOADING_RESULTS:
         with open(os.path.join(models_dir, 'sindy_model_upper_body.pkl'), 'rb') as f:
             sindy_model['upper_body'] = dill.load(f)
 
+        toc = time.time() - tic
+        minutes, seconds = divmod(toc, 60)
+        print(f"SINDy initialization took {minutes:.0f} minutes and {seconds:.2f} seconds.")
+
+
     # Run the loop for the IDENTIFICATION subjects
     print("\nTraining dataset:")
     ukf_predictor.run_filtering_loop(
-        X_train_list, time_train_list, train_traj_idx, PRED_HORIZONS, PREDICT_K_STEPS,
+        X_train_list, time_train_list, train_traj_idx, num_pred_steps, PREDICT_K_STEPS,
         dim_x, dim_z, p_idx, DT,
         N_VAR_PER_KPT, N_DIM_PER_KPT, N_KPTS,
         init_P, VAR_MEAS_KPT, VAR_Q_ACC,
@@ -337,6 +372,8 @@ if not LOADING_RESULTS:
         output_dir,
         'train',
         space=SPACE,
+        n_joints=N_JOINTS,
+        n_params=N_PARAM,
         param_idx=param_idx,
         sindy_model=sindy_model
     )
@@ -344,7 +381,7 @@ if not LOADING_RESULTS:
     # Run the loop for the VALIDATION subjects
     print("\n\nTesting dataset:")
     ukf_predictor.run_filtering_loop(
-        X_test_list, time_test_list, test_traj_idx, PRED_HORIZONS, PREDICT_K_STEPS,
+        X_test_list, time_test_list, test_traj_idx, num_pred_steps, PREDICT_K_STEPS,
         dim_x, dim_z, p_idx, DT,
         N_VAR_PER_KPT, N_DIM_PER_KPT, N_KPTS,
         init_P, VAR_MEAS_KPT, VAR_Q_ACC,
@@ -355,6 +392,8 @@ if not LOADING_RESULTS:
         output_dir,
         'test',
         space=SPACE,
+        n_joints=N_JOINTS,
+        n_params=N_PARAM,
         param_idx=param_idx,
         sindy_model=sindy_model
     )
@@ -383,7 +422,7 @@ for file in pickle_files:
 conf_names = output_column_names['filtered_joint_column_names'] \
     if SPACE == 'joint' else output_column_names['filtered_column_names']
 
-for horizon in PRED_HORIZONS:
+for horizon in num_pred_steps:
     # Load the results for the IDENTIFICATION subjects
     for file in pickle_files:
         if f'train_filtering_results_{horizon}_steps' in file:

scripts/data_analysis/nstep_ukf_imm_estimator.py
@@ -1,12 +1,17 @@
 import os, copy, torch, tqdm, time, pickle
+from multipledispatch import dispatch
 import numpy as np
 import pandas as pd
+
 import human_model_binding as hkm # human kinematic model
-from filterpy.kalman import UnscentedKalmanFilter, MerweScaledSigmaPoints, IMMEstimator, unscented_transform
+from filterpy.kalman import UnscentedKalmanFilter, MerweScaledSigmaPoints, IMMEstimator
 from filterpy.common import Q_discrete_white_noise
 from scipy.linalg import block_diag
-from multipledispatch import dispatch
 from utils import compute_state_indices
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import entropy
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -114,7 +119,7 @@ def initialize_filters(dim_x, dim_z, p_idx, dt,
         @dispatch(np.ndarray, param=np.ndarray)
         def hx(x: np.ndarray, param: np.ndarray):
             kp_in_ext = hkm.Keypoints()
-            human_kinematic_model.forward_kinematics(x, param, kp_in_ext) # type: ignore (assertion already ensures that human_kinematic_model is not None)
+            human_kinematic_model.forward_kinematics(x[p_idx], param, kp_in_ext) # type: ignore (assertion already ensures that human_kinematic_model is not None)
             return kp_in_ext.get_keypoints()
     else:
         raise ValueError('Invalid space. Choose between "cartesian" or "joint".')
@@ -314,32 +319,39 @@ def propagate_covariance(dim_x, dim_z, ukf, body_model, p_idx, param_idx):
     if (np.isnan(ukf.x)).any() or (np.isnan(ukf.P)).any():
         return np.nan * np.ones(dim_z), np.nan * np.ones(dim_z * dim_z)
     
+    # Extract the position and parameter states and their covariance
+    q_pos = ukf.x[p_idx]
+    param = ukf.x[param_idx]
+    P_pos = ukf.P[np.ix_(p_idx, p_idx)]
+    P_param = ukf.P[np.ix_(param_idx, param_idx)]
+    q_pos_size = len(q_pos)
+    param_size = len(param)
+
+    state = np.concatenate((q_pos, param))
+    cov = np.block([
+        [P_pos, ukf.P[np.ix_(p_idx, param_idx)]],
+        [ukf.P[np.ix_(param_idx, p_idx)], P_param]
+    ])
+
     # Sigma points for the UKF
-    sigma_fn = MerweScaledSigmaPoints(n=dim_x, alpha=.1, beta=2., kappa=1.)
+    sigma_fn = MerweScaledSigmaPoints(n=q_pos_size+param_size, alpha=.1, beta=2., kappa=1.)
 
     # Calculate sigma points for given mean and covariance
-    sigmas = sigma_fn.sigma_points(ukf.x, ukf.P)
+    sigmas = sigma_fn.sigma_points(state, cov)
 
     sigmas_h = []
     kpts = hkm.Keypoints()
     for s in sigmas:
-        body_model.forward_kinematics(s[p_idx], s[param_idx], kpts)
+        # Normalize the quaternion
+        quat = s[3:7]
+        s[3:7] = quat / np.linalg.norm(quat)
+
+        body_model.forward_kinematics(s[:q_pos_size], s[q_pos_size:], kpts)
         sigmas_h.append(kpts.get_keypoints())
 
     sigmas_h = np.atleast_2d(sigmas_h)
 
-    # Mean and covariance of prediction passed through unscented transform
-    if isinstance(ukf, IMMEstimator):
-        # Select the first filter in the bank (arbitrary choice)
-        Wm = ukf.filters[0].Wm
-        Wc = ukf.filters[0].Wc
-        R = ukf.filters[0].R
-    else:
-        Wm = ukf.Wm
-        Wc = ukf.Wc
-        R = ukf.R
-
-    # kpts_mean, kpts_cov = unscented_transform(sigmas_h, Wm, Wc, R)
+    # Compute the mean and covariance of the propagated sigma points
     kpts_mean = np.mean(sigmas_h, axis=0)
     kpts_cov = np.cov(sigmas_h.T)
     
@@ -626,6 +638,8 @@ def run_filtering_loop(X_train_list, time_train_list, train_traj_idx,
                        results_dir,
                        filename,
                        space='cartesian',
+                       n_joints=28,
+                       n_params=8,
                        param_idx=np.array([]),
                        sindy_model=None):
     
@@ -656,14 +670,15 @@ def run_filtering_loop(X_train_list, time_train_list, train_traj_idx,
         for traj_idx in range(len(X_train_list)):
             traj_info = train_traj_idx[traj_idx]
             subject_id, velocity, task, instruction = traj_info['subject'], traj_info['velocity'], traj_info['task'], traj_info['instruction']
-            print(f'\n[ITER {hor_idx*len(X_train_list) + traj_idx + 1} / {len(X_train_list)*len(pred_horizons)}]: processing {traj_info} for {k} steps ahead...')
+            print(f'\n[ITER {hor_idx*len(X_train_list) + traj_idx + 1} / {len(X_train_list)*len(pred_horizons)}]: ',
+                  f'processing {traj_info} for {k} steps ahead (dt = {dt})...')
 
             # Create a HumanBodyModel object if the space is 'joint'
             if space == 'joint':
                 body_model = hkm.Human28DOF()
 
                 # Initialize joint limits
-                qbounds = [hkm.JointLimits(-np.pi, np.pi)]*28
+                qbounds = [hkm.JointLimits(-np.pi, np.pi)]*n_joints
 
                 # Set the shoulder rot y joint limits
                 qbounds[12] = hkm.JointLimits(-np.pi/2, np.pi/2) # right shoulder
@@ -782,8 +797,8 @@ def run_filtering_loop(X_train_list, time_train_list, train_traj_idx,
                         kp_in_ext.set_keypoints(kpts)
                         
                         # Update the HumanBodyModel with the current joint angles
-                        q = np.zeros(28)
-                        param = np.zeros(8)
+                        q = np.zeros(n_joints)
+                        param = np.zeros(n_params)
                         if body_model is not None:
                             q, param = body_model.inverse_kinematics(kp_in_ext, qbounds, q, param) 
 
@@ -881,6 +896,7 @@ def run_filtering_loop(X_train_list, time_train_list, train_traj_idx,
                                     for f in bank_pred.filters:
                                         f.P = get_near_psd(f.P)
 
+                                    # Open-loop prediction
                                     ca_ukf_pred.predict()
                                     bank_pred.predict()
 
@@ -893,9 +909,9 @@ def run_filtering_loop(X_train_list, time_train_list, train_traj_idx,
                                     if space == 'joint':
                                         # Propagate the covariance matrices from joint space to Cartesian space
                                         ca_ukf_pred_kpt, ca_ukf_pred_kpt_cov = \
-                                            propagate_covariance(dim_x, dim_z, ca_ukf, body_model, p_idx, param_idx)
+                                            propagate_covariance(dim_x, dim_z, ca_ukf_pred, body_model, p_idx, param_idx)
                                         bank_pred_kpt, bank_pred_kpt_cov = \
-                                            propagate_covariance(dim_x, dim_z, bank, body_model, p_idx, param_idx)
+                                            propagate_covariance(dim_x, dim_z, bank_pred, body_model, p_idx, param_idx)
                                         
                                         uxs_ca_kpt_pred[i].append(ca_ukf_pred_kpt)
                                         uxs_bank_kpt_pred[i].append(bank_pred_kpt)
@@ -1381,3 +1397,183 @@ def run_filtering_loop(X_train_list, time_train_list, train_traj_idx,
 
 #     return measurement_split, filtering_results, prediction_results
 ### =================================== ###
+
+
+def tune_init_variance_joints(X_train_list, time_train_list, train_traj_idx,
+                              dt, selected_kpt_names, selected_keypoints_for_kinematic_model,
+                              n_joints, n_params, num_mc_samples, kpt_variance):
+        
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    from scipy.stats import shapiro, entropy
+
+    # Define the column names for the measurements
+    measurements_column_names = ['kp{}_{}'.format(i, axis)
+                                for i in selected_keypoints_for_kinematic_model
+                                for axis in ['x', 'y', 'z']]
+
+    # Create dictionary to store results
+    measurement_split = {}   # dictionary of DataFrames with the measurements split by task
+
+    # Loop over the training trajectories
+    for traj_idx in range(len(X_train_list)):
+        traj_info = train_traj_idx[traj_idx]
+        subject_id, velocity, task, instruction = traj_info['subject'], traj_info['velocity'], traj_info['task'], traj_info['instruction']
+        print(f'\n[ITER {traj_idx + 1} / {len(X_train_list)}]: processing {traj_info}...')
+
+        # Create a HumanBodyModel object
+        body_model = hkm.Human28DOF()
+
+        # Initialize joint limits
+        qbounds = [hkm.JointLimits(-np.pi, np.pi)]*n_joints
+
+        # Set the shoulder rot y joint limits
+        qbounds[12] = hkm.JointLimits(-np.pi/2, np.pi/2) # right shoulder
+        qbounds[16] = hkm.JointLimits(-np.pi/2, np.pi/2) # left shoulder
+        qbounds[20] = hkm.JointLimits(-np.pi/2, np.pi/2) # right hip
+        qbounds[24] = hkm.JointLimits(-np.pi/2, np.pi/2) # left hip
+
+        # Initialize Keypoints object
+        kp_in_ext = hkm.Keypoints()
+
+        # Get the measurements
+        meas = X_train_list[traj_idx]
+        time_info = time_train_list[traj_idx]
+
+        zs = pd.DataFrame(meas, columns=measurements_column_names)
+        zs['timestamp'] = pd.to_timedelta(time_info, unit='s')
+        zs.set_index('timestamp', inplace=True)
+
+        measurement_split[(subject_id, velocity, task, instruction)] = zs
+
+        # If zs only contains NaN values, skip the current iteration
+        nan_idxs = zs.apply(lambda x: all(pd.isna(x)), axis=1)
+        if nan_idxs.all():
+            print(f"Skipping {subject_id} - {velocity} - {task}.")
+            continue
+
+        # Define times
+        t = zs.index[0]
+        t_end = zs.index[-1]
+        t_incr = pd.Timedelta(seconds=dt)
+
+        print("Start time:", t, " [s] | End time:", t_end, " [s]")
+
+        # Main loop
+        total_iterations = int((t_end - t) / t_incr) + 1
+        pbar = tqdm.tqdm(total=total_iterations)
+
+        start_time = time.time()
+        while t <= t_end:
+            # Get the current measurement
+            z = zs.loc[t].values
+
+            # Generate num_mc_samples random samples for each element of z following a normal distribution
+            # Mean: z, Standard deviation: sqrt(kpt_variance)
+            z_samples = np.random.normal(z, np.sqrt(kpt_variance), (num_mc_samples, len(z)))
+            z_samples_gaussian = np.random.normal(z, np.sqrt(kpt_variance), (num_mc_samples, len(z)))
+
+            q_samples = np.zeros((num_mc_samples, n_joints))
+            param_samples = np.zeros((num_mc_samples, n_params))
+            for i, sample in enumerate(z_samples):
+                # Update the Keypoints object with the current joint angles
+                kpts = {}
+                for key, value in selected_kpt_names.items():
+                    kpts[value] = sample[selected_keypoints_for_kinematic_model.index(key)*3 : \
+                                         selected_keypoints_for_kinematic_model.index(key)*3+3]
+
+                kp_in_ext.set_keypoints(kpts)
+                        
+                # Update the HumanBodyModel with the current joint angles
+                q = np.zeros(n_joints)
+                param = np.zeros(n_params)
+                q, param = body_model.inverse_kinematics(kp_in_ext, qbounds, q, param)
+
+                # Store the joint angles and parameters
+                q_samples[i] = q
+                param_samples[i] = param
+
+            q_mean = np.mean(q_samples, axis=0)
+            q_std = np.std(q_samples, axis=0)
+            q_samples_gaussian = np.random.normal(q_mean, q_std, (num_mc_samples, n_joints))
+
+            param_mean = np.mean(param_samples, axis=0)
+            param_std = np.std(param_samples, axis=0)
+            param_samples_gaussian = np.random.normal(param_mean, param_std, (num_mc_samples, n_params))
+
+            # Store z_samples, q_samples, and param_samples in a single DataFrame
+            z_samples_df = pd.DataFrame(z_samples, columns=measurements_column_names)
+            z_samples_gaussian_df = pd.DataFrame(z_samples_gaussian, columns=measurements_column_names)
+            q_samples_df = pd.DataFrame(q_samples, columns=[f'q_{i}' for i in range(n_joints)])
+            q_samples_gaussian_df = pd.DataFrame(q_samples_gaussian, columns=[f'q_{i}' for i in range(n_joints)])
+            param_samples_df = pd.DataFrame(param_samples, columns=[f'param_{i}' for i in range(n_params)])
+            param_samples_gaussian_df = pd.DataFrame(param_samples_gaussian, columns=[f'param_{i}' for i in range(n_params)])
+
+            # z_samples_df #
+            plot_kde_histograms(z_samples_df, z_samples_gaussian_df, measurements_column_names,
+                                subplot_rows=5, subplot_cols=8)
+
+            # q_samples_df #
+            plot_kde_histograms(q_samples_df, q_samples_gaussian_df, [f'q_{i}' for i in range(n_joints)],
+                                subplot_rows=4, subplot_cols=7)
+
+            # param_samples_df #
+            plot_kde_histograms(param_samples_df, param_samples_gaussian_df, [f'param_{i}' for i in range(n_params)],
+                                subplot_rows=2, subplot_cols=4)
+            
+            plt.show()
+        
+            t += t_incr                        
+            pbar.update()
+
+        pbar.close()
+        print(f"Average loop frequency: {(total_iterations / (time.time() - start_time)):.2f} Hz")
+
+    return 0, 1
+
+
+
+def plot_kde_histograms(data, data_ideal, column_names, subplot_rows, subplot_cols,
+                        figsize=(1.92, 1.08), bw_adjust=0.5, bins=30):
+    # Create a subplot grid
+    fig, axes = plt.subplots(subplot_rows, subplot_cols, figsize=figsize)
+    axes = axes.flatten()
+
+    for i, column in enumerate(column_names):
+        # Plot histogram of the samples and the KDE
+        sns.histplot(data=data, x=column, kde=False, ax=axes[i], bins=bins, stat="density", alpha=0.3)
+        
+        # Plot measured KDE
+        p_kde = sns.kdeplot(data=data, x=column, ax=axes[i], color='blue', linestyle='-')
+        p_pdf = p_kde.get_lines()[0].get_data()[1]
+
+        # Plot ideal KDE
+        q_kde = sns.kdeplot(data=data_ideal, x=column, ax=axes[i], color='red', linestyle='--')
+        q_pdf = q_kde.get_lines()[0].get_data()[1]
+
+        # Normalize the PDFs
+        p_pdf = p_pdf / np.sum(p_pdf)
+        q_pdf = q_pdf / np.sum(q_pdf)
+
+        # Compute KL divergence
+        kl_div = entropy(p_pdf, q_pdf)
+        
+        # Set plot title and remove x and y labels
+        axes[i].set_title(column)
+        axes[i].set_xlabel('')
+        axes[i].set_ylabel('')
+                
+        # Display the KL divergence in the top right corner of the plot
+        axes[i].text(0.95, 0.95, f'KL: {kl_div:.2f}',
+                     ha='right', va='top', transform=axes[i].transAxes)
+
+    # Hide any unused subplots
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
+
+    # Add horizontal space between subplots
+    fig.subplots_adjust(hspace=1.0)
+
+    # Adjust layout
+    plt.tight_layout()
+    plt.draw()

scripts/data_analysis/plot_results.py
@@ -21,26 +21,12 @@ N_VAR_PER_KPT = 3                               # position, velocity, accelerati
 N_DIM_PER_KPT = 3                               # x, y, z
 
 SELECTED_KEYPOINTS_FOR_KINEMATIC_MODEL = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]    # select which keypoints to consider for the kinematic model
-# SELECTED_KPT_NAMES = {
-#     0: "head",
-#     5: "left_shoulder",
-#     6: "left_elbow",
-#     7: "left_wrist",
-#     11: "left_hip",
-#     12: "left_knee",
-#     13: "left_ankle",
-#     2: "right_shoulder",
-#     3: "right_elbow",
-#     4: "right_wrist",
-#     8: "right_hip",
-#     9: "right_knee",
-#     10: "right_ankle"
-# }
 
-SAMPLING_TIME = 0.1 # seconds
-PREDICT_K_STEPS = True
-PREDICTION_STEPS = 5 # steps
 NUM_SIGMAS = 1 # number of standard deviations for the covariance cones
+PREDICT_K_STEPS = True
+SAMPLING_TIME = 0.1 # seconds
+PREDICTION_HORIZON = 0.5 # seconds
+prediction_steps = int(np.round(PREDICTION_HORIZON / SAMPLING_TIME))
 
 TRAINORTEST = 'train'
 
@@ -55,8 +41,8 @@ VELOCITIES = ['FAST']#['SLOW', 'FAST']
 TASKS = ['PICK-&-PLACE']#['PICK-&-PLACE', 'WALKING']
 SPACES_COMPUTE = ['cartesian']#['cartesian', 'joint']
 SPACES_EVAL = ['cartesian']#['cartesian', 'joint']
-SUBJECT = 'sub_4'
-INSTRUCTION = 1
+SUBJECT = 'sub_11'
+INSTRUCTION = 3
 
 # Get the current working directory
 cwd = os.getcwd()
@@ -101,7 +87,7 @@ for space_c in SPACES_COMPUTE:
 
                         # Load the filtering results
                         file = os.path.join(data_dir,
-                                            f'{TRAINORTEST}_filtering_results_{PREDICTION_STEPS}_steps_{space_c}_.pkl')
+                                            f'{TRAINORTEST}_filtering_results_{prediction_steps}_steps_{space_c}_.pkl')
                         
                         with open(file, 'rb') as f:
                             print(f"\nLoading file: {file}...")
@@ -109,7 +95,7 @@ for space_c in SPACES_COMPUTE:
 
                         # Load the prediction results
                         file = os.path.join(data_dir,
-                                            f'{TRAINORTEST}_prediction_results_{PREDICTION_STEPS}_steps_{space_c}_.pkl')
+                                            f'{TRAINORTEST}_prediction_results_{prediction_steps}_steps_{space_c}_.pkl')
                         
                         with open(file, 'rb') as f:
                             print(f"Loading file: {file}...")
@@ -121,7 +107,13 @@ for space_c in SPACES_COMPUTE:
                                 y_axes_lim = [0.12, 0.85]
                                 start_meas = 0.0
                                 end_meas = 10.0
-                                kpt = 4
+                                if INSTRUCTION == 1:
+                                    kpt = 4 # right wrist
+                                elif INSTRUCTION == 3:
+                                    kpt = 7 # left wrist
+                                else:
+                                    kpt = 4 # by default choose right wrist
+                                    raise ValueError('CHECK')
                                 dim = 'x'
                             elif task == 'WALKING':
                                 y_axes_lim = [-1.0, 1.5]
@@ -134,7 +126,13 @@ for space_c in SPACES_COMPUTE:
                                 y_axes_lim = [-0.3, 1.15]
                                 start_meas = 0.0
                                 end_meas = 10.0
-                                kpt = 4
+                                if INSTRUCTION == 1:
+                                    kpt = 4 # right wrist
+                                elif INSTRUCTION == 3:
+                                    kpt = 7 # left wrist
+                                else:
+                                    kpt = 4 # by default choose right wrist
+                                    raise ValueError('CHECK')
                                 dim = 'x'
                             elif task == 'WALKING':
                                 y_axes_lim = [-1, 1.25]
@@ -156,7 +154,7 @@ for space_c in SPACES_COMPUTE:
                     # Define a list of pd.Timedelta to uniformly sample the time range
                     selected_timestamps = [pd.Timedelta(seconds=round(s, 1))
                                            for s in np.arange(start_meas,
-                                                              end_meas-SAMPLING_TIME*PREDICTION_STEPS,
+                                                              end_meas-PREDICTION_HORIZON,
                                                               CONE_STEP)]
 
                     # Define the time range to plot the time series
@@ -169,6 +167,6 @@ for space_c in SPACES_COMPUTE:
 
                     plot_covariance_cone(measurements, filtering_results, prediction_results,
                                          SUBJECT, velocity, task, INSTRUCTION, kpt, kpt_idx, dim, DIM_TYPE,
-                                         dim_x, N_VAR_PER_KPT, N_DIM_PER_KPT, SAMPLING_TIME, PREDICTION_STEPS,
-                                         PREDICTION_STEPS, selected_timestamps, selected_range, filter_type, y_axes_lim,
+                                         dim_x, N_VAR_PER_KPT, N_DIM_PER_KPT, SAMPLING_TIME, PREDICT_K_STEPS,
+                                         prediction_steps, selected_timestamps, selected_range, filter_type, y_axes_lim,
                                          plot_dir, num_sigmas=NUM_SIGMAS)

scripts/data_analysis/utils.py
@@ -13,7 +13,7 @@ DEFAULT_PLOTLY_COLORS=['rgb(31, 119, 180)', 'rgb(255, 127, 14)',
 alpha = 0.2
 DEFAULT_PLOTLY_COLORS_ALPHA = [color.replace('rgb', 'rgba').replace(')', f', {alpha})') for color in DEFAULT_PLOTLY_COLORS] # used for 5-step ahead prediction CI
 
-alpha = 0.3
+alpha = 0.1
 DEFAULT_PLOTLY_COLORS_ALPHA_HIGH = [color.replace('rgb', 'rgba').replace(')', f', {alpha})') for color in DEFAULT_PLOTLY_COLORS] # used for covariance cones
 
 
@@ -379,7 +379,7 @@ def plot_covariance_cone(measurements, filtering_results, prediction_results,
                             mode='lines',
                             name=f'Prediction at current time',
                             line=dict(color=DEFAULT_PLOTLY_COLORS[2],
-                                      width=3),
+                                      width=2),
                             showlegend=ca_cone_legend_not_plotted
             )
             ca_cone_legend_not_plotted = False
@@ -409,7 +409,7 @@ def plot_covariance_cone(measurements, filtering_results, prediction_results,
                             mode='lines',
                             name=f'Prediction at current time',
                             line=dict(color=DEFAULT_PLOTLY_COLORS[2],
-                                      width=3),
+                                      width=2),
                             showlegend=imm_cone_legend_not_plotted
             )
             imm_cone_legend_not_plotted = False
@@ -438,12 +438,6 @@ def plot_covariance_cone(measurements, filtering_results, prediction_results,
 
     # Plot k-step ahead predictions with their confidence intervals
     if predict_k_steps:
-        # if filter_type == 'CA':
-        #     # remove the element k-step ahead prediction at time t=0.4s for visualization purposes
-        #     kpred_cut.loc[pd.Timedelta(seconds=0.4), '_'.join(('ca', state))] = np.nan
-        #     kpred_cut.loc[pd.Timedelta(seconds=0.4), '_'.join(('ca', state, 'ucl'))] = np.nan
-        #     kpred_cut.loc[pd.Timedelta(seconds=0.4), '_'.join(('ca', state, 'lcl'))] = np.nan
-
         fig.add_scatter(x=kpred_seconds,
                         y=kpred_cut['_'.join((filter_type.lower(), state))],
                         mode='lines',
@@ -522,5 +516,5 @@ def plot_covariance_cone(measurements, filtering_results, prediction_results,
     plot_name = '_'.join([subject, velocity, task, state, "dt", str(dt),
                           "k", str(k), str(dim_type), "pred", str(predict_k_steps),
                           "cone", str(filter_type), "sigmas", str(num_sigmas)])
-    fig.write_html(os.path.join(plot_dir, plot_name+ '_PAPER.html')) # interactive plot
-    fig.write_image(os.path.join(plot_dir, plot_name+ '_PAPER.pdf')) # static plot
+    fig.write_html(os.path.join(plot_dir, plot_name+ '.html')) # interactive plot
+    fig.write_image(os.path.join(plot_dir, plot_name+ '.pdf')) # static plot